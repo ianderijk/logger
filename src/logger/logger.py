0bf6071daf25d4fe364c181c0dfd0ba23da5362b
Ian de Rijk <106862077+ianderijk@users.noreply.github.com>
2026-08-21 22:30:38 +0100

    Streaming (#1)
    * WIP
    
    * cleanup

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,36 +1,38 @@
 import logging
-import platform
-import os
-from datetime import datetime
-from pathlib import Path
+import atexit
+from logging.handlers import SocketHandler, DEFAULT_TCP_LOGGING_PORT
+from typing import Literal
 
 
 class ProjectLogger:
-    def __init__(self, project_name: str, level: str = "DEBUG") -> None:
-        self.project_name = project_name
-        self.level = level
-        self.logger = logging.getLogger(self.project_name)
-        self.logger.setLevel(self.level)
-        self.logger.propagate = False
-        self._create_project_log_directory()
+    def __init__(
+        self,
+        name: str,
+        level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "DEBUG",
+    ) -> None:
+        self.logger = logging.getLogger(name)
+        self.level = self._set_log_level(level)
+        self.socket_handler = None
+
         self._set_logger_format()
+        self._attach_listener()
 
-    def _build_log_path(self) -> None:
-        device = platform.node()
-        session_timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        if "raspberry" in device:
-            logs_root = Path("/media/ianderijk/Backup/LogStore")
-        else:
-            logs_root = Path("/media/idr/ExtDrive/LogStore")
-        log_path = logs_root / self.project_name / f"{session_timestamp}.log"
-        self.log_path = log_path
+        atexit.register(self._cleanup)
 
-    def _create_project_log_directory(self) -> None:
-        self._build_log_path()
-        log_dir = self.log_path.parent
-        if log_dir.exists():
-            return
-        os.mkdir(log_dir)
+    def _set_log_level(
+        self, level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+    ):
+        match level:
+            case "DEBUG":
+                return logging.DEBUG
+            case "INFO":
+                return logging.INFO
+            case "WARNING":
+                return logging.WARNING
+            case "ERROR":
+                return logging.ERROR
+            case "CRITICAL":
+                return logging.CRITICAL
 
     def _set_logger_format(self) -> None:
         formatter = logging.Formatter(
@@ -42,10 +44,14 @@ class ProjectLogger:
         console_handler.setFormatter(formatter)
         self.logger.addHandler(console_handler)
 
-        file_handler = logging.FileHandler(self.log_path)
-        file_handler.setLevel(self.level)
-        file_handler.setFormatter(formatter)
-        self.logger.addHandler(file_handler)
+    def _attach_listener(self) -> None:
+        self.socket_handler = SocketHandler("localhost", DEFAULT_TCP_LOGGING_PORT)
+        self.logger.addHandler(self.socket_handler)
+
+    def _cleanup(self) -> None:
+        if self.socket_handler:
+            self.socket_handler.close()
+            self.logger.removeHandler(self.socket_handler)
 
     def info(self, msg: str) -> None:
         self.logger.info(msg)
